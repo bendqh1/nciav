6f2f50ba85ac9e2f3c6302d7932574b575c9afc6
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2024-10-21 11:54:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 window.addEventListener('DOMContentLoaded', ()=>{
 
     window.setInterval(()=>{
-        document.querySelectorAll('*').forEach(element=>{
-            element.style.filter = "grayscale(100%)";
-        });
-    }, 1);
-  
-    window.setInterval(()=>{
-        if ( document.querySelector('img') ) {
-            document.querySelectorAll('img').forEach(image=>{
-                image.remove();
-            });
-        };
-    }, 1);
 
-window.setInterval(()=>{
-    if ( document.querySelector('audio') ) {
+      document.querySelectorAll('*').forEach(element=>{
+        element.style.filter = "grayscale(100%)";
+      });
+
+      if ( document.querySelector('img') ) {
+        document.querySelectorAll('img').forEach(image=>{
+          image.remove();
+        });
+      };
+
+      if ( document.querySelector('audio') ) {
         document.querySelectorAll('audio').forEach(audio=>{
             audio.pause();
             audio.currentTime = 0;
             audio.remove();
         });
+      };
+
+      if ( document.querySelector('video') ) {
+        document.querySelectorAll('video').forEach(video=>{
+            video.pause();
+            video.remove();
+      });
     };
-}, 1);
 
-    window.setInterval(()=>{
-        if ( document.querySelector('video') ) {
-            document.querySelectorAll('video').forEach(video=>{
-                video.pause();
-                video.remove();
-            });
-        };
-    }, 1);
-
+  }, 1);
+  
 });
